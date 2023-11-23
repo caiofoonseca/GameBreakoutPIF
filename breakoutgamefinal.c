ce0f89f14e75efbc6e66a3c8c33dec703fb3593f
caiofoonseca <136398896+caiofoonseca@users.noreply.github.com>
2023-11-23 00:13:30 -0300

    breakoutgamefinal.c

--- breakoutgamefinal.c
+++ breakoutgamefinal.c
@@ -39,7 +39,6 @@ void inicializarBlocos(struct Bloco blocos[], int quantidadeTijolos) {
     blocos[i].quebrado = 0;
   }
 
-  // Defina o número de fileiras de tijolos com base na quantidade desejada
   int numFileiras = quantidadeTijolos / NUM_BLOCOS_LINHA;
   int larguraBloco = LARGURA_TELA / NUM_BLOCOS_LINHA;
   int alturaBloco = 1;
@@ -57,13 +56,11 @@ void inicializarBlocos(struct Bloco blocos[], int quantidadeTijolos) {
   }
 }
 void limparTela() {
-  printf("\033[H\033[J"); // Código ANSI para limpar a tela
+  printf("\033[H\033[J");
 }
 void desenhar(struct Paleta *paleta, struct Bola *bola, struct Bloco blocos[]) {
   limparTela();
 
-  // Calcular a largura de um bloco com base no tamanho da tela e no número de
-  // blocos por linha
   int larguraBloco = LARGURA_TELA / NUM_BLOCOS_LINHA;
 
   for (int i = 0; i < ALTURA_TELA; i++) {
@@ -73,22 +70,20 @@ void desenhar(struct Paleta *paleta, struct Bola *bola, struct Bloco blocos[]) {
       } else if (i == bola->y && j == bola->x) {
         printf("O");
       } else {
-        // Calcule as coordenadas do bloco atual
         int colunaBloco = j / larguraBloco;
         int linhaBloco = i / 2;
         int indiceBloco = linhaBloco * NUM_BLOCOS_LINHA + colunaBloco;
 
-        // Verifique se estamos dentro dos limites dos blocos
         if (colunaBloco >= 0 && colunaBloco < NUM_BLOCOS_LINHA &&
             linhaBloco >= 0 && linhaBloco < NUM_BLOCOS_COLUNA) {
           if (!blocos[indiceBloco].quebrado) {
-            printf("#"); // Tijolo não quebrado
+            printf("#");
           } else {
-            printf(" "); // Espaço vazio onde o tijolo foi quebrado
+            printf(" ");
           }
-          j += larguraBloco - 1; // Pule as células ocupadas pelos blocos
+          j += larguraBloco - 1;
         } else {
-          printf(" "); // Espaço vazio fora dos blocos
+          printf(" ");
         }
       }
     }
@@ -98,15 +93,15 @@ void desenhar(struct Paleta *paleta, struct Bola *bola, struct Bloco blocos[]) {
 int jogarJogo(int velocidade, int quantidadeTijolos) {
   struct Paleta paleta = {LARGURA_TELA / 2 - 2,
                           ALTURA_TELA -
-                              2}; // A paleta agora fica uma linha acima
+                              2};
   struct Bola bola = {LARGURA_TELA / 2, ALTURA_TELA / 2, 1, -1};
   struct Bloco blocos[NUM_BLOCOS];
 
   inicializarBlocos(blocos, quantidadeTijolos);
-  keyboardInit(); // Inicializa o teclado para captura não bloqueante
+  keyboardInit();
 
   int blocosQuebrados = 0;
-  int jogoAtivo = 1; // Variável para controlar se o jogo está ativo
+  int jogoAtivo = 1;
 
   while (jogoAtivo) {
     if (keyhit()) {
@@ -145,14 +140,13 @@ int jogarJogo(int velocidade, int quantidadeTijolos) {
     desenhar(&paleta, &bola, blocos);
 
     if (bola.y >= ALTURA_TELA - 1) {
-      // A bola tocou no chão
-      jogoAtivo = 0; // O jogo não está mais ativo
+      jogoAtivo = 0;
     }
 
     usleep(velocidade);
   }
 
-  keyboardDestroy(); // Restaura as configurações do teclado
+  keyboardDestroy();
   return blocosQuebrados;
 }
 
@@ -166,8 +160,7 @@ void exibirMenu() {
   printf("3. Sair\n");
   printf("===================================\n");
   printf("Escolha uma opcao: ");
-  fflush(stdout); // Garante que a saída do printf seja enviada imediatamente
-                  // para o terminal
+  fflush(stdout);
 }
 
 int obterEscolhaMenu() {
@@ -177,11 +170,10 @@ int obterEscolhaMenu() {
     if (keyhit()) {
       char tecla = readch();
       if (tecla >= '1' && tecla <= '3') {
-        escolha = tecla - '0'; // Converte o caractere numérico para o número
-                               // inteiro correspondente
+        escolha = tecla - '0';
       }
     }
-    usleep(10000); // Aguarda um pouco para não sobrecarregar a CPU
+    usleep(10000);
   }
   keyboardDestroy();
   return escolha;
@@ -194,8 +186,7 @@ void exibirMenuModoJogo(int *escolhaModo) {
   printf("3. Dificil\n");
   printf("===================================\n");
   printf("Escolha uma opcao: ");
-  fflush(stdout); // Garante que a saída do printf seja enviada imediatamente
-                  // para o terminal
+  fflush(stdout);
 
   *escolhaModo = obterEscolhaMenu();
 }
@@ -209,29 +200,28 @@ void digitarNome(char nome[]) {
     if (keyhit()) {
       char tecla = readch();
       if (tecla == '\n') {
-        break; // Se o jogador pressionar Enter, encerra a digitação
+        break;
       } else if (tecla == 127) {
-        // Se o jogador pressionar Backspace, apaga o último caractere
         if (i > 0) {
           i--;
           nome[i] = '\0';
-          printf("\b \b"); // Apaga o caractere digitado no terminal
+          printf("\b \b");
           fflush(stdout);
         }
-      } else if (i < 49) { // Evita estouro de buffer
+      } else if (i < 49) {
         nome[i] = tecla;
         i++;
-        nome[i] = '\0';      // Garante que a string seja terminada corretamente
-        printf("%c", tecla); // Exibe o caractere digitado no terminal
+        nome[i] = '\0';
+        printf("%c", tecla); 
         fflush(stdout);
       }
     }
-    usleep(10000); // Aguarda um pouco para não sobrecarregar a CPU
+    usleep(10000);
   }
 }
 
 int main() {
-  srand(time(NULL)); // Inicialize a semente do gerador de números aleatórios
+  srand(time(NULL));
 
   struct Jogador *jogadores = malloc(50 * sizeof(struct Jogador));
   if (jogadores == NULL) {
@@ -240,13 +230,12 @@ int main() {
   }
   int numJogadores = 0;
 
-  keyboardInit(); // Inicializa o teclado para captura não bloqueante
+  keyboardInit();
 
   while (1) {
     exibirMenu();
 
-    int escolha = obterEscolhaMenu(); // Usa a função obterEscolhaMenu para
-                                      // obter a escolha do usuário
+    int escolha = obterEscolhaMenu();
 
     switch (escolha) {
     case 1: {
@@ -258,20 +247,20 @@ int main() {
       switch (escolhaModo) {
       case 1:
         quantidadeTijolos = NUM_BLOCOS_FACIL;
-        velocidade = 200000; // 200ms
+        velocidade = 200000;
         break;
       case 2:
         quantidadeTijolos = NUM_BLOCOS_MEDIO;
-        velocidade = 170000; // 170 ms
+        velocidade = 170000;
         break;
       case 3:
         quantidadeTijolos = NUM_BLOCOS_DIFICIL;
-        velocidade = 150000; // 150 ms
+        velocidade = 150000;
         break;
       default:
         printf("Escolha inválida. Modo Fácil selecionado por padrão.\n");
         quantidadeTijolos = NUM_BLOCOS_FACIL;
-        velocidade = 50000; // 50 ms
+        velocidade = 50000;
         break;
       }
 
@@ -280,11 +269,11 @@ int main() {
 
       limparTela();
       printf("Preparando o jogo...");
-      fflush(stdout); // Força a saída do buffer
-      sleep(3);       // Aguarde 3 segundos antes de começar o jogo
+      fflush(stdout);
+      sleep(3);     
 
       strcpy(jogadores[numJogadores].nome,
-             nome); // Copia o nome digitado para a estrutura
+             nome);
       jogadores[numJogadores].pontuacao =
           jogarJogo(velocidade, quantidadeTijolos);
       numJogadores++;
@@ -292,7 +281,6 @@ int main() {
       break;
     }
     case 2: {
-      // Ordenar jogadores com base na pontuação (do maior para o menor)
       for (int i = 0; i < numJogadores - 1; i++) {
         for (int j = i + 1; j < numJogadores; j++) {
           if (jogadores[i].pontuacao < jogadores[j].pontuacao) {
@@ -310,12 +298,12 @@ int main() {
       }
 
       printf("\nClique Enter para voltar ao menu principal...");
-      fflush(stdout); // Força a saída do buffer
+      fflush(stdout);
       while (1) {
         if (keyhit()) {
           char tecla = readch();
           if (tecla == '\n') {
-            break; // Sai do loop quando o usuário pressiona Enter
+            break;
           }
         }
       }
@@ -323,12 +311,12 @@ int main() {
     }
     case 3:
       printf("\nJogo finalizado. Obrigado por jogar!\n");
-      free(jogadores);   // Libera a memória alocada dinamicamente
-      keyboardDestroy(); // Restaura as configurações do teclado
-      exit(0);           // Sai do programa
+      free(jogadores);   
+      keyboardDestroy();
+      exit(0);           
     }
   }
-  keyboardDestroy(); // Nunca será alcançado se o loop for infinito
-  free(jogadores);   // Nunca será alcançado se o loop for infinito
-  return 0;          // Nunca será alcançado se o loop for infinito
+  keyboardDestroy(); 
+  free(jogadores);  
+  return 0; 
 }
